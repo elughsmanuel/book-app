9d402d185117de2e296dc992d0c7bf136eeefd17
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-15 10:15:23 +0100

    Media Responsiveness

--- src/js/index.js
+++ src/js/index.js
@@ -38,3 +38,33 @@ bookmarkPage.addEventListener('click', function() {
     aboutPage.classList.remove('active');
     bookmarkPage.classList.add('active');
 });
+
+
+
+// MOBILE NAVIGATION BAR
+
+var homePage2 = document.querySelector('#home-page-2');
+var aboutPage2 = document.querySelector('#about-page-2');
+var bookmarkPage2 = document.querySelector('#bookmark-page-2');
+
+var homeBox2 = document.querySelector('.home-box');
+var aboutBox2 = document.querySelector('.about-box');
+var bookmarkBox2 = document.querySelector('.bookmark-box');
+
+homePage2.addEventListener('click', function() {
+    homeBox2.classList.remove('home-box-display');
+    aboutBox2.classList.add('about-box-display');
+    bookmarkBox2.classList.add('bookmark-box-display');
+});
+
+aboutPage2.addEventListener('click', function() {
+    homeBox2.classList.add('home-box-display');
+    aboutBox2.classList.remove('about-box-display');
+    bookmarkBox2.classList.add('bookmark-box-display');
+});
+
+bookmarkPage2.addEventListener('click', function() {
+    homeBox2.classList.add('home-box-display');
+    aboutBox2.classList.add('about-box-display');
+    bookmarkBox2.classList.remove('bookmark-box-display');
+});
